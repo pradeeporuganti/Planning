ed751705912b6725a05bae44f643a5dee8a36c46
pradeeporuganti <50497698+pradeeporuganti@users.noreply.github.com>
2021-08-17 19:15:55 -0400

    Add files via upload
    updated sampling function to accept samples outside of obstacles. 
    testing collision detection function.

--- myRRT.py
+++ myRRT.py
@@ -1,20 +1,18 @@
-import math
 import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
 import numpy as np
 
 class Node():
 
-    def __init__(self, x, y) -> None:
+    def __init__(self, x, y, parent = None):
         self.x = x
         self.y = y
-        self.parent = None
+        self.parent = parent
 
 
 # Generates map with random obstacles
 class Map(Node):
 
-    def __init__(self, start, goal ,mapLength, mapHeight, obsLength, obsHeight, numObs = -1):
+    def __init__(self, start, goal ,mapLength, mapHeight, obsLength, obsHeight, numObs = -1, goalFlag = False):
         self.start = start
         self.goal = goal
         self.mapLength = mapLength
@@ -23,8 +21,17 @@ class Map(Node):
         self.numObs = int(numObs)
         self.obsLength = obsLength
         self.obsHeight = obsHeight
+        self.goalFlag = goalFlag
+
+        # init tree
+        self.tree = []
+        self.tree.append(Node(self.start[0], self.start[1], None))
+
+        # center point of obstacle ellipse
         self.obsLoc_x = np.zeros([1, self.numObs])
         self.obsLoc_y = np.zeros([1, self.numObs])
+
+        # ellipse x-y points
         self.xobs = np.zeros([self.numObs, 100])
         self.yobs = np.zeros([self.numObs, 100])
         self.genObs()
@@ -37,11 +44,14 @@ class Map(Node):
     def genObs(self):
         n = self.numObs-1
         while not n < 0:
-            self.obsLoc_x[n] = np.random.uniform(0, self.mapLength - self.obsLength)
-            self.obsLoc_y[n] = np.random.uniform(0, self.mapHeight - self.obsHeight)
-            self.obs.append((self.obsLoc_x[n], self.obsLoc_y[n]))
+            self.obsLoc_x[0][n] = np.random.uniform(0, self.mapLength - self.obsLength)
+            self.obsLoc_y[0][n] = np.random.uniform(0, self.mapHeight - self.obsHeight)
+            # make sure that start and end goal are not within obstacles
+            if  (((self.start[0] - self.obsLoc_x[0][n])**2/(self.obsLength)**2) + ((self.start[1] - self.obsLoc_y[0][n])**2/(self.obsHeight)**2)) > 1 \
+                and (((self.goal[0] - self.obsLoc_x[0][n])**2/(self.obsLength)**2) + ((self.goal[1] - self.obsLoc_y[0][n])**2/(self.obsHeight)**2)) > 1:
+                self.obs.append((self.obsLoc_x[0][n], self.obsLoc_y[0][n] ))
             n = n - 1
-
+        
     # Draws current map
     def drawMap(self):
         fig, ax = plt.subplots(1)
@@ -50,15 +60,17 @@ class Map(Node):
         theta = np.linspace(0, 2*np.pi, 100)
         for n in range(0, self.numObs):
             for t in range(0, len(theta)):
-                self.xobs[t][n] = self.obsLength * np.cos(math.radians(theta[t])) + self.obsLoc_x[n]
-                self.yobs[t][n] = self.obsHeight * np.sin(math.radians(theta[t])) + self.obsLoc_y[n]
-        print(self.xobs[:][0])
+                self.xobs[n][t] = self.obsLength * np.cos(theta[t]) + self.obsLoc_x[0][n]
+                self.yobs[n][t] = self.obsHeight * np.sin(theta[t]) + self.obsLoc_y[0][n]
         for n in range(0, self.numObs):
-            for i in range(0, len(theta)):
-                ax.plot(self.xobs[i][n], self.yobs[i][n])
+                ax.plot(self.xobs[n][:], self.yobs[n][:], 'k-')
 
+    def drawEdge(self):
+        pass
+
+    # generate a map object to pass around
     def genMap(self):
-        return [self.start, self.goal, self.mapLength, self.mapHeight, self.numObs, self.obs, self.obsLength, self.obsHeight]
+        return [self.start, self.goal, self.mapLength, self.mapHeight, self.numObs, self.obs, self.obsLength, self.obsHeight, self.goalFlag]
         
 class RRT(Map):
 
@@ -72,26 +84,84 @@ class RRT(Map):
         self.obs = m[5]
         self.obsLength = m[6]
         self.obsHeight = m[7]
+        self.goalFlag = m[8]
     
+    # get sample point but dont sample from within obstacle
     def sampleEnv(self):
-        #check = [False]*self.numObs
-        #while not all(check):
-        samp_x = np.random.uniform(0, self.mapLength)
-        samp_y = np.random.uniform(0, self.mapHeight)
-            #for i in range(0, self.numObs):
-            #    if samp_x - self.obs[i][0] > self.obsLength and samp_y - self.obs[i][1] > self.obsHeight:
-            #        check[i] = True
+        check = [False]*self.numObs
+        while not all(check):
+            samp_x = np.random.uniform(0, self.mapLength)
+            samp_y = np.random.uniform(0, self.mapHeight)
+            for n in range(0, self.numObs):
+                if (((samp_x - self.obs[n][0])**2/(self.obsLength)**2) + ((samp_y - self.obs[n][1])**2/(self.obsHeight)**2)) > 1:
+                    check[n] = True
         return((samp_x, samp_y))
+
+    # Find the node in the tree that is nearest to the sampled node
+    #def nearest(self):
+    #    while not self.goalFlag:
+    #        nrand = self.sampleEnv()
+    #        nnear = self.start
+    #        near_dist = self.distance(self.start, nrand)
+    #        for n in self.tree:
+    #            if self.distance((n.x, n.y), nrand) < near_dist:
+    #                nnear = (n.x, n.y)
+    #            else:
+    #                pass
+    #        if self.checkCollision(nnear, nrand):       # True if no collisions with obstacles
+    #            if self.distance(nnear, nrand) > 10:    # make sure new node is lower than 10 units distance
+    #                v = list(nnear)-list(nrand)
+    #                u = v/np.linalg.norm(v, 2)
+    #                nrand = list(nnear) + 10*u          # new point atmost 10 unit distance from nearest node on tree
+    #            else:
+    #                pass
+    #            self.tree.append(Node(nrand[0], nrand[1], nnear))   # add random point to tree
+    #            if self.distance(nrand, self.goal) < 5:     # check if random point close enough to goal
+    #                self.goalFlag = True
+    #            else:
+    #                pass
+    #        else:
+    #            pass
+
+    def checkCollision(self):
+        for n in range(0, self.numObs):
+            
+            
+            
+            
+            # generate line equation
+            m = (nnear[1]-samp_y)/(nnear[0]-samp_x)
+            c = samp_y - m*samp_x
+            D = self.obsLength**2*m**2 + self.obsHeight**2 - c**2 
+            if D < 0:
+                if self.distance()
+                self.tree.append(Node(samp_x, samp_y, nnear))
+            else:
+                pass
+            if self.distance((samp_x, samp_y), (self.goal[0], self.goal[1])) < 5:
+                self.tree.append(self.goal[0], self.goal[1], (samp_x, samp_y))
+                goalFlag= True
+
+    
+    def distance(p1, p2):
+        return np.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+
+    # Find path to the goal
+    #def finalPath:
+        
 
 def main():
     start = (5, 40)
     goal = (100, 5)
-    m = Map(start, goal, 100, 50, 4.5, 1.5, 2)
+    m = Map(start, goal, 100, 50, 4.5, 1.5, 10)
     ax = plt.gca()
     p = RRT(m)
     iter = 0
+    prev = start
     while (iter < 500):
-        ax.plot(p.sampleEnv()[0],p.sampleEnv()[1] , 'ro', markersize=2)
+        (px, py) = p.sampleEnv()
+        if RRT.checkCollision(prev, (px, py)):
+            Map.drawEdge(prev, (px, py))
         plt.pause(0.005)
         iter = iter + 1
     plt.show()
--- tester.py
+++ tester.py
@@ -1,34 +1,53 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-theta = np.linspace(0, 2*np.pi, 100)
-numobs = 3
+def checkCollision(p1, p2, obs):
+    obsx = obs[0]; obsy = obs[1]; a= obs[2]; b = obs[3]
+    m = (p2[1] - p1[1])/(p2[0]-p1[0])
+    c_org = p1[1]-m*p1[0]
+    c_new = c_org + m*obsx - obsy
+    D = a**2*m**2 + b**2 - c_new**2 
+    if D < 0:
+        return True
+    else:
+        return False
 
-obsx = np.array([[]])
-obsy = np.array([[]])
+def main():
+    theta = np.linspace(0, 2*np.pi, 100)
+    numobs = 1
+    a = 10; b = 4
 
-for i in range(0, numobs):
-    obsx = np.append(obsx, [int(np.random.uniform(0, 100))])
-    obsy = np.append(obsy, [int(np.random.uniform(0, 100))])
+    obsx = np.array([[]])
+    obsy = np.array([[]])
 
-print(obsx)
-print(obsy)
+    p1 = (int(np.random.uniform(0, 100)), int(np.random.uniform(0, 100)))
+    p2 = (int(np.random.uniform(0, 100)), int(np.random.uniform(0, 100)))
+    #p1 = (-5, -5)
+    #p2 = (-20, -20)
 
-xobs = np.zeros([numobs, 100])
-yobs = np.zeros([numobs, 100])
+    for i in range(0, numobs):
+        obsx = np.append(obsx, int(np.random.uniform(0, 100)))
+        obsy = np.append(obsy, int(np.random.uniform(0, 100)))
 
-for n in range(0, numobs):
-    for t in range(0, len(theta)):
-        xobs[n][t] = 10 * np.cos(theta[t]) + obsx[n]
-        yobs[n][t] = 4 * np.sin(theta[t]) + obsy[n]
+    xobs = np.zeros([numobs, 100])
+    yobs = np.zeros([numobs, 100])
 
-fig, ax = plt.subplots(1)
-ax.plot(xobs[0][:], yobs[0][:],'r-')
-ax.plot(xobs[1][:], yobs[1][:],'k-')
-ax.plot(xobs[2][:], yobs[2][:],'b-')
+    for n in range(0, numobs):
+        for t in range(0, len(theta)):
+            xobs[n][t] = a * np.cos(theta[t]) + obsx[n]
+            yobs[n][t] = b * np.sin(theta[t]) + obsy[n]
 
-#for n in range(0, numobs):
+    fig, ax = plt.subplots(1)
+    ax.plot(p1[0], p1[1], marker = '.', markersize = 10)
+    ax.plot(p2[0], p2[1], marker = '.', markersize = 10)
+    ax.plot([p1[0], p2[0]], [p1[1], p2[1]], 'r-')
+    for n in range(0, numobs):
+        ax.plot(xobs[n][:], yobs[n][:],'r-')
+        
+    c = checkCollision(p1, p2, (obsx[n], obsy[n], a, b))
+    print(c)
 
+    plt.show()
 
-plt.show()
-
+if __name__ == "__main__":
+    main()
